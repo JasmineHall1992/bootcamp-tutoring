6e62b8590ca27945c489885d3379f6b62ac5eaf9
JasmineHall1992 <jasminehall5935@gmail.com>
2024-12-18 15:31:21 -0600

    jmh

--- week-1/day-2/loops/loops.js
+++ week-1/day-2/loops/loops.js
@@ -1,8 +1,19 @@
 
 // Problem #1 //
-function logLocationAndTime(array){
-   
-}
+function logLocationAndTime(students){
+        // Iterate BACKWARDS through the array
+        for (let i = students.length - 1; i >= 0; i--) {
+            // Make variables for location and time
+            let local = students[i].location; 
+            let zone = students[i].timeZone;
+    
+            // Make a template literal to log
+            let output = `${local} - ${zone}`;
+            console.log(output);
+        }
+    }
+    
+
 
 // Problem #2 //
 function logHalf(array){
